4cfcfec21bfd3ce30dea2f431f96d0dff59b000e
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-30 17:08:30 +0330

    add UserController with endpoint to retrieve users

---
+++ src/main/java/com/parsa/todospring/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.parsa.todospring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @GetMapping("/getusers")
+    public void getUsers(){
+
+    }
+}
