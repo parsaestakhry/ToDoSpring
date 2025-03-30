ca2da8b06b7bf8f349398171e36989868ad8bc89
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-30 21:41:10 +0330

    add UserService: implement service layer for user management

---
+++ src/main/java/com/parsa/todospring/service/UserService.java
@@ -0,0 +1,30 @@
+package com.parsa.todospring.service;
+
+
+import com.parsa.todospring.model.User;
+import com.parsa.todospring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserRepository getUserRepository() {
+        return userRepository;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user){
+        userRepository.saveUser(user);
+    }
+}
