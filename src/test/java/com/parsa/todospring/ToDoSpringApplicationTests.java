586d89c9a65393e862b0e8b9c9e294e6ecc0535b
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-30 16:29:07 +0330

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/com/parsa/todospring/ToDoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsa.todospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
