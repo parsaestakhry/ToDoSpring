ade4afae522b4fcb3b219863de62f7aa3afb0af3
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 13:14:36 +0330

    refactor HelloController: remove unused import for RequestMapping

--- src/main/java/com/parsa/todospring/controller/HelloController.java
+++ src/main/java/com/parsa/todospring/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.parsa.todospring.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
