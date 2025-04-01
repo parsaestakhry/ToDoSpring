17de4d5a45502deddeb225c951e20eb55258802e
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 18:43:03 +0330

    add updateToDoList endpoint: implement method to update a ToDoList entry by ID

--- src/main/java/com/parsa/todospring/controller/ToDoListController.java
+++ src/main/java/com/parsa/todospring/controller/ToDoListController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -38,7 +40,14 @@ public class ToDoListController {
     public ToDoList getToDoList(@PathVariable int id) {
         return toDoListService.getToDoList(id);
     }
+
+    @PutMapping("/updatelist/{id}")
+    public ToDoList updateToDoList(@RequestBody ToDoList toDoList) {
+        return toDoListService.updateToDoList(toDoList);
+    }
     
+
+
 
 
     
