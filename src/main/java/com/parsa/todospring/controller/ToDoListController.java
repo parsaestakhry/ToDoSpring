12646bf0a888695a3969e29f8f8bec5e9dca588b
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 18:36:07 +0330

    add getToDoList endpoint: implement method to retrieve a specific ToDoList entry by ID

--- src/main/java/com/parsa/todospring/controller/ToDoListController.java
+++ src/main/java/com/parsa/todospring/controller/ToDoListController.java
@@ -1,7 +1,7 @@
 package com.parsa.todospring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.parsa.todospring.model.ToDoList;
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -31,6 +32,11 @@ public class ToDoListController {
     @GetMapping("/getlists")
     public List<ToDoList> getToDoLists() {
         return toDoListService.getToDoLists();
+    }
+
+    @GetMapping("/getlist/{id}")
+    public ToDoList getToDoList(@PathVariable int id) {
+        return toDoListService.getToDoList(id);
     }
     
 
