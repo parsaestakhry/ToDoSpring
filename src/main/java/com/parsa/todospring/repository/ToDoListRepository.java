0b13f6ec7a00f6d161c7d7b9972fbf908935b92a
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 17:51:15 +0330

    add ToDoListRepository: implement repository class for ToDoList data access

---
+++ src/main/java/com/parsa/todospring/repository/ToDoListRepository.java
@@ -0,0 +1,25 @@
+package com.parsa.todospring.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ToDoListRepository {
+    
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    
+
+    
+
+}
