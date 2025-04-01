ba2774f9d0dcca5c37d094dc0a0e9aaf8671fc18
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 18:30:59 +0330

    refactor ToDoListService: update getToDoList method to return a list of ToDoList entries

--- src/main/java/com/parsa/todospring/service/ToDoListService.java
+++ src/main/java/com/parsa/todospring/service/ToDoListService.java
@@ -1,5 +1,7 @@
 package com.parsa.todospring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,8 +30,8 @@ public class ToDoListService {
         toDoListRepository.createToDoList(toDoList);
     }
 
-    public void getToDoList() {
-        toDoListRepository.getToDolists();
+    public List<ToDoList> getToDoLists() {
+        return toDoListRepository.getToDolists();
     }
 
     
