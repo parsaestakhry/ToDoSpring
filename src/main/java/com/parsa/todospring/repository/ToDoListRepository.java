73846ea05269ef16bbceafb3cb550c4bffbfa39b
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 18:18:23 +0330

    add getToDolists method: implement method to retrieve all ToDoList entries from the database

--- src/main/java/com/parsa/todospring/repository/ToDoListRepository.java
+++ src/main/java/com/parsa/todospring/repository/ToDoListRepository.java
@@ -1,9 +1,12 @@
 package com.parsa.todospring.repository;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
 import com.parsa.todospring.model.ToDoList;
 
 @Repository
@@ -22,8 +25,23 @@ public class ToDoListRepository {
 
     public void createToDoList(ToDoList todolist) {
         String sql = "INSERT INTO todo_lists(name,created_at, id, user_id) VALUES (?, ?, ?, ?)";
-        int rows = jdbcTemplate.update(sql, todolist.getName(), todolist.getCreated_at(), todolist.getId(), todolist.getUser_id());
+        int rows = jdbcTemplate.update(sql, todolist.getName(), todolist.getCreated_at(), todolist.getId(),
+                todolist.getUser_id());
         System.out.println("Rows Affected:" + rows);
+    }
+    
+    public List<ToDoList> getToDolists() {
+        String sql = "SELECT * FROM todo_lists";
+        RowMapper<ToDoList> mapper = (rs, rowNum) -> {
+            ToDoList todolist = new ToDoList();
+            todolist.setId(rs.getInt("id"));
+            todolist.setCreated_at(rs.getDate("created_at"));
+            todolist.setName(rs.getString("name"));
+            todolist.setUser_id(rs.getInt("user_id"));
+            return todolist;
+        };
+
+        return jdbcTemplate.query(sql, mapper);
     }
 
 }
