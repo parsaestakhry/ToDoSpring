30820d410f423da3cb3bcbf7daa24ca980cb68bf
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 18:00:10 +0330

    add createToDoList method: implement method to insert new ToDoList entries into the database

--- src/main/java/com/parsa/todospring/repository/ToDoListRepository.java
+++ src/main/java/com/parsa/todospring/repository/ToDoListRepository.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.parsa.todospring.model.ToDoList;
+
 @Repository
 public class ToDoListRepository {
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -18,8 +20,10 @@ public class ToDoListRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    
-
-    
+    public void createToDoList(ToDoList todolist) {
+        String sql = "INSERT INTO todo_lists(name,created_at, id, user_id) VALUES (?, ?, ?, ?)";
+        int rows = jdbcTemplate.update(sql, todolist.getName(), todolist.getCreated_at(), todolist.getId(), todolist.getUser_id());
+        System.out.println("Rows Affected:" + rows);
+    }
 
 }
