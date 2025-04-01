ed126cc66edd15ff0789610abb57e0781c4bc5a4
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-01 18:48:37 +0330

    add deleteToDoList method: implement functionality to delete a ToDoList entry by ID

--- src/main/java/com/parsa/todospring/controller/ToDoListController.java
+++ src/main/java/com/parsa/todospring/controller/ToDoListController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -45,7 +46,13 @@ public class ToDoListController {
     public ToDoList updateToDoList(@RequestBody ToDoList toDoList) {
         return toDoListService.updateToDoList(toDoList);
     }
+
+    @DeleteMapping("/deletelist/{id}")
+    public boolean deleteToDoList(@PathVariable int id) {
+        return toDoListService.deleteToDoList(id);
+    }
     
+
 
 
 
