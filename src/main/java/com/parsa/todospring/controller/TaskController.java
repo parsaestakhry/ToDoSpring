337b90843a8a422393a3a4e5db291fba165f07eb
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-02 19:01:40 +0330

    add updateTask functionality: implement task update in TaskController

--- src/main/java/com/parsa/todospring/controller/TaskController.java
+++ src/main/java/com/parsa/todospring/controller/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -38,6 +39,14 @@ public class TaskController {
     public Task getTask(@PathVariable() int id) {
         return taskService.getTask(id);
     }
+
+    @PutMapping("/updatetask")
+    public Task updateTask(@RequestBody Task task) {
+        return taskService.updateTask(task);
+    }
+
+
+
 
 
     
