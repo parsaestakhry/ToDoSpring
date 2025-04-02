1cbe504b9c699364459ab90290194493d5cff8bc
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-04-02 18:57:30 +0330

    add getTask functionality: implement retrieval of a single Task by ID in TaskController

--- src/main/java/com/parsa/todospring/controller/TaskController.java
+++ src/main/java/com/parsa/todospring/controller/TaskController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -32,6 +33,14 @@ public class TaskController {
     public List<Task> getTasks() {
         return taskService.getTasks();
     }
+
+    @GetMapping("/gettask/{id}")
+    public Task getTask(@PathVariable() int id) {
+        return taskService.getTask(id);
+    }
+
+
+    
     
 
 
